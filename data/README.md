025599d9027999b938b4730daacaa8a24fa03b6a
Junnian Liu <liuj4467@Junnians-MacBook-Air.local>
2026-03-02 12:59:38 -0800

    initial commit

---
+++ data/README.md
@@ -0,0 +1,10 @@
+// TODO: Add description of WiLI-2018 dataset...
+
+Run bash script:
+bash scripts/download_data.sh
+
+Place the following files in data/:
+  - x_train.txt   (training sentences, one per line)
+  - y_train.txt   (language labels, one per line)
+  - x_test.txt
+  - y_test.txt
---
+++ scripts/download_data.sh
@@ -0,0 +1,11 @@
+# TODO: add documentation describing what this file does...
+
+#!/bin/bash
+
+mkdir -p data
+
+# flags: -L -> follow redirects, -o -> output to specified file path.
+curl -L -o data/wili-2018.zip https://zenodo.org/record/841984/files/wili-2018.zip
+unzip data/wili-2018.zip -d data/
+rm data/wili-2018.zip
+echo "Done. Run scripts/preprocess.py next."
